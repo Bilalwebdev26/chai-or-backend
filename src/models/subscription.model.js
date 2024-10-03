6fa250dc7b14a3c0823ff4843a78efe7bda97fa9
bilalwebdev26 <f2022266833@umt.edu.pk>
2024-10-03 10:30:48 -0700

    arrange subs model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 
-const subscriptionSchema = new mongoose.Schema({
-   subscriber:{
-    type:mongoose.Schema.Types.ObjectId,//One who is subscribing
-    ref:"User"
-   },
-   channel:{
-    type:mongoose.Schema.Types.ObjectId,//One who is subscribing
-    ref:"User"
-   }
-},{timestamps:true})
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId, //One who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: mongoose.Schema.Types.ObjectId, //One who is subscribing
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-export const Subscription = mongoose.model("Subscription",subscriptionSchema)
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
