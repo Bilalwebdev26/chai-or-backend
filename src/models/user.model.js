e420071fb5a07de290ea8064ae4770ff286aa263
bilalwebdev26 <f2022266833@umt.edu.pk>
2024-10-04 01:57:34 -0700

    arrange models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,84 +1,86 @@
 import mongoose from "mongoose";
-import bycrypt from "bcrypt"
+import bycrypt from "bcrypt";
 import jwt from "jsonwebtoken";
- const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        lowercase:true,
-        trim:true,
-        unique:true,
-        index:true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      index: true,
     },
-    email:{
-        type:String,
-        unique:true,
-        lowercase:true,
-        required:true,
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: true,
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
-    fullName:{
-        type:String,
-        required:true
+    fullName: {
+      type: String,
+      required: true,
     },
-    avatar:{
-        type:String,
-        required:true
+    avatar: {
+      type: String,
+      required: true,
     },
-    coverImage:{
-        type:String
+    coverImage: {
+      type: String,
     },
-    refreshToken:{
-        type:String
+    refreshToken: {
+      type: String,
     },
-    watchHistory:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"Video"
-        }
-    ]
- },{timestamps:true})
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-userSchema.pre("save",async function (next) {
-    if(!this.isModified("password")){
-        return next()
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  let saltrounds = 10;
+  this.password = await bycrypt.hash(this.password, saltrounds);
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bycrypt.compare(password, this.password); //1st password 2nd encrypted password
+};
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     }
-    let saltrounds = 10
-    this.password = await bycrypt.hash(this.password,saltrounds)
-    next()
-})
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bycrypt.compare(password,this.password)//1st password 2nd encrypted password
-}
-userSchema.methods.generateAccessToken = function(){
-    return jwt.sign(
-        {
-            _id:this._id,
-            email:this.email,
-            username:this.username,
-            fullname:this.fullname,
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn:process.env.ACCESS_TOKEN_EXPIRY,
-        }
-    )
-}
+  );
+};
 
-userSchema.methods.generateRefreshToken = function(){
-    return jwt.sign(
-        {
-            _id:this._id,
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn:process.env.REFRESH_TOKEN_EXPIRY,
-        }
-    )
-}
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
-
- export const User = mongoose.model("User",userSchema)
+export const User = mongoose.model("User", userSchema);
