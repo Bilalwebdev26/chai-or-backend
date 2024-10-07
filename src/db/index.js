27508bd6be6ad9e3376ee82734913b3285ba639c
bilalwebdev26 <f2022266833@umt.edu.pk>
2024-10-07 08:39:21 -0700

    arrange db/index file

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
-import {DB_NAME} from "../constants.js"
+import { DB_NAME } from "../constants.js";
 
-const conectDB = async()=>{
-   try {
-   const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-   console.log(`MONGODB connected successfully !! DB HOST : ${connectionInstance.connection.host}`)
-   } catch (error) {
-     console.log(`MONGODB connection failed : ${error}`)
-     process.exit(1)
-   }
-}
+const conectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(`Connection : ${connectionInstance}`);
+    console.log(`Connection . connect : ${connectionInstance.connect}`);
+    console.log(
+      `MONGODB connected successfully !! DB HOST : ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log(`MONGODB connection failed : ${error}`);
+    process.exit(1);
+  }
+};
 
-export default  conectDB ;
+export default conectDB;
