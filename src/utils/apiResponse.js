e1160ec626c18e60d18879262d25032934edf6ed
bilalwebdev26 <f2022266833@umt.edu.pk>
2024-12-22 10:35:59 -0800

    api response

--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -1,10 +1,10 @@
 class ApiResponse {
-    constructor(statuscode,message="Success",data){
-         this.statuscode = statuscode,
-         this.data = data,
-         this.message = message,
-         this.success = statuscode < 400
-    }
+  constructor(statuscode, message = "Success", data) {
+    (this.statuscode = statuscode),
+      (this.data = data),
+      (this.message = message),
+      (this.success = statuscode < 400);
+  }
 }
 
-export { ApiResponse }
+export { ApiResponse };
